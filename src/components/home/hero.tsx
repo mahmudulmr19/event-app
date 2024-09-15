76be1f0f0bf4144c25b65877fcb69fdb14325ec3
Mahmudul Hasan <mahmudul.dev7@gmail.com>
2024-09-15 19:17:29 +0600

    feat: add SponsoredList component with Title, Gold, and Silver sponsor cards to enhance sponsorship visibility in hero section

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,14 +1,15 @@
 import { Navbar } from "@/components/shared";
 import { Container } from "@/components/ui";
 import { SpeakerIcon } from "@/components/icons";
+import { SponsoredList } from "./sponsored-list";
 
 export function Hero() {
   return (
     <div className="bg-cover bg-no-repeat bg-top bg-[url(/bg-hero.svg)]">
       <Navbar />
-      <Container className="py-10 md:py-16 lg:py-20 xl:py-28 relative">
-        <div className="absolute bottom-[70%] left-8 size-52 max-lg:hidden bg-[#7AFF06] transform translate-y-1/2 blur-[400px]" />
-        <div className="absolute bottom-[70%] right-8 size-52 max-lg:hidden bg-[#4EBBEB] transform translate-y-1/2 blur-[400px]" />
+      <Container className="py-10 md:py-16 lg:py-20 xl:py-28 relative z-10">
+        <div className="absolute bottom-[70%] left-8 size-52 max-lg:hidden bg-[#7AFF06] transform translate-y-1/2 blur-[400px] -z-[1]" />
+        <div className="absolute bottom-[70%] right-8 size-52 max-lg:hidden bg-[#4EBBEB] transform translate-y-1/2 blur-[400px] -z-[1]" />
         <div className="max-w-screen-md mx-auto flex flex-col items-center">
           <div className="flex items-center gap-x-3">
             <SpeakerIcon />
@@ -28,6 +29,7 @@ export function Hero() {
             Grab a ticket
           </a>
         </div>
+        <SponsoredList />
       </Container>
     </div>
   );
