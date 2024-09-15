86addc449713b74b414dc7ad19c4888ec48425a7
Mahmudul Hasan <mahmudul.dev7@gmail.com>
2024-09-15 21:34:20 +0600

    updated

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -4,7 +4,7 @@ export function Banner() {
   return (
     <Container className="pb-10 md:pb-14">
       <div className="py-10 md:py-16 lg:py-20 bg-[url(/bg-banner.png)] bg-no-repeat bg-cover rounded-2xl flex flex-col items-center gap-y-5">
-        <div className="section-title">
+        <div className="section-title max-w-2xl text-center">
           Tickets are selling fast, grab now before we run out!
         </div>
         <a
