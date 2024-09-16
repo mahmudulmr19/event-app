748976239677d3a2e6f022f8f2d6e79fcb2d593f
Mahmudul Hasan <mahmudul.dev7@gmail.com>
2024-09-17 00:21:02 +0600

    feat: update tickets count API to support path revalidation via request object for improved cache handling

--- src/app/api/tickets-count/route.ts
+++ src/app/api/tickets-count/route.ts
@@ -1,6 +1,10 @@
 import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const path = req.nextUrl.searchParams.get("path") || "/";
+  revalidatePath(path);
   const data = await db.ticket.count({ where: { status: "success" } });
   return new Response(JSON.stringify(data), { status: 200 });
 }
